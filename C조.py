24581d8e88165eb02c3e1ebe5d7945dc61b4205c
Jiwon Lee <53148677+GodJiLee@users.noreply.github.com>
2021-11-11 10:54:50 +0900

    11/11 A팀 다이얼로그 기능 반영 - 이지원

--- C조.py
+++ C조.py
@@ -72,7 +72,7 @@ class DataFrameModel(QAbstractTableModel):
 
 class MyApp(QWidget):
 
-    def __init__(self):
+    def __init__(self): 
         super().__init__()
         self.init_UI()
         ##Initialize Variables
