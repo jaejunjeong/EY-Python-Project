08f49a8fa695e73525282510c4d7906e72330e44
Jiwon Lee <53148677+GodJiLee@users.noreply.github.com>
2021-11-14 15:21:53 +0900

    11/14 dialog5 기능 변경 - 이지원

--- C조.py
+++ C조.py
@@ -4,7 +4,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 import pyodbc
-import pandas as pd
+import pandas as pd 
 
 
 class DataFrameModel(QAbstractTableModel):
